3dae1966d7ec75981420d2580b3f34d3a6e68e36
Mirza3262204 <muhammad.alfarrel@binus.ac.id>
2026-06-02 12:54:15 +0700

    CP Ke-4 02-06_2026
    1. Set enemy route waypoint

--- Assets/Script/EnemyBehavior.cs
+++ Assets/Script/EnemyBehavior.cs
@@ -1,12 +1,85 @@
 using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    private NavMeshAgent _agent;
+    public Transform PatrolRoute;
+    private List<Transform> _locations = new List<Transform>();
+    private int _locationIndex = 0;
+    private Transform _player;
+    private GameBehavior _gameManager;
+    private int _enemyLives = 3;
+    public int EnemyLives
+    {
+        get { return _enemyLives; }
+        private set
+        {
+            _enemyLives = value;
+            if (_enemyLives <= 0)
+            {
+                Destroy(this.gameObject);
+                Debug.Log("Enemy Down!");
+            }
+        }
+
+    }
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+
+        _player = GameObject.Find("Player").transform;
+
+        if (PatrolRoute != null)
+        {
+            foreach (Transform child in PatrolRoute)
+            {
+                _locations.Add(child);
+            }
+        }
+
+        if (_locations.Count > 0)
+        {
+            MoveToNextPatrolLocation();
+        }
+
+        _gameManager = GameObject.Find("Game Manager").GetComponent<GameBehavior>();
+    }
+
+    void Update()
+    {
+
+        if (_locations.Count > 0 && _agent.remainingDistance < 0.5f && !_agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+
+    void MoveToNextPatrolLocation()
+    {
+        if (_locations.Count == 0) return;
+
+        _agent.destination = _locations[_locationIndex].position;
+        _locationIndex = (_locationIndex + 1) % _locations.Count;
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
         {
             Debug.Log("Player detected - attack!");
+
+            if (_gameManager != null)
+            {
+                _gameManager.HP -= 2;
+                    Debug.Log("Player HP: " + _gameManager.HP);
+            }
+
+            if (_agent != null && _player != null)
+            {
+                _agent.destination = _player.position;
+            }
         }
     }
 
@@ -15,6 +88,20 @@ public class EnemyBehavior : MonoBehaviour
         if (other.name == "Player")
         {
             Debug.Log("Player out of range, resume patrol");
+
+            if (_locations.Count > 0)
+            {
+                MoveToNextPatrolLocation();
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Bullet(Clone)")
+        {
+            EnemyLives -= 1;
+            Debug.Log("Critical Hit! Enemy Lives: " + _enemyLives);
         }
     }
 }
---
+++ Assets/Script/GameBehavior.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameBehavior : MonoBehaviour
+{
+    private int _itemsCollected = 0;
+    private int _playerHP = 10;
+    public int MaxItems = 4;
+
+    public TMP_Text HealthText;
+    public TMP_Text ItemText;
+    public TMP_Text ProgressText;
+    public Button WinButton;
+    public Button LossButton;
+
+    void Start()
+    {
+        ItemText.text = "Items: " + _itemsCollected;
+        HealthText.text = "Health: " + _playerHP;
+
+        if (WinButton != null)
+            WinButton.gameObject.SetActive(false);
+
+        if (LossButton != null) LossButton.gameObject.SetActive(false);
+    }
+
+    public int Items
+    {
+        get { return _itemsCollected; }
+        set
+        {
+            _itemsCollected = value;
+            ItemText.text = "Items: " + _itemsCollected;
+
+            if (_itemsCollected >= MaxItems)
+            {
+                ProgressText.text = "You've found all the items!";
+                if (WinButton != null)
+                    WinButton.gameObject.SetActive(true);
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                int remaining = MaxItems - _itemsCollected;
+                ProgressText.text = "Item found, only " + remaining + " more to go!";
+            }
+        }
+    }
+
+    public int HP
+    {
+        get { return _playerHP; }
+        set
+        {
+            _playerHP = value;
+            HealthText.text = "Health: "  + _playerHP;
+            Debug.LogFormat("Lives: {0}", _playerHP);
+
+            if (_playerHP <= 0)
+            {
+                UpdateScene("You want another life with that?");
+                if (LossButton != null) LossButton.gameObject.SetActive(true);
+            }
+            else
+            {
+                ProgressText.text = "Ouch... that's got hurt";
+            }
+        }
+    }
+
+    public void UpdateScene(string updatedText)
+    {
+        ProgressText.text = updatedText;
+        Time.timeScale = 0f;
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+    }
+}
